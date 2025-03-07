9d323a42105e1df2822254e35734718996cf4b7e
Manav Bilakhia <manav.mitesh@gmail.com>
2025-03-07 13:57:11 -0500

    gen lund maybe fixed wait for andrey approval?

--- utils/hipo2root/genlund.py
+++ utils/hipo2root/genlund.py
@@ -1,3 +1,4 @@
+import ROOT
 import os
 import random
 
@@ -31,41 +32,45 @@ def generate_lund_files(output_folder, num_files, num_events_per_file):
                 f.write(f"{num_particles} {mass_number_of_target} {atomic_number_of_target} {target_polarization} {beam_polarization} {beam_type} {beam_energy} {interacted_nucleon} {process_id} {event_weight}\n")
                 
                 # === Generate random parameters for the trigger electron ===
-                index_el = 1
-                lifetime_el = -1
-                type_el = 1 # propagated in geant4
-                particle_id_el = 11
-                index_parent_el = 0
-                index_daughter_el = 0
-                electron_px = random.uniform(-1, 1)
-                electron_py = random.uniform(-1, 1)
-                electron_pz = random.uniform(8, 10.6)  # Simulating 10.6 GeV energy
-                electron_mass = 0.000511
-                electron_energy = (electron_px**2 + electron_py**2 + electron_pz**2 + electron_mass**2)**0.5
-                electron_vx = 0  # Vertex x-coordinate
-                electron_vy = 0  # Vertex y-coordinate
-                electron_vz = random.gauss(-3, 2.5)  # Random vertex z-coordinate with mean -3 cm and sigma 2.5 cm  
+                index_electron = 1
+                lifetime_electron = -1
+                type_electron = 1 # propagated in geant4
+                particle_id_electron = 11
+                index_parent_electron = 0
+                index_daughter_elctron = 0
+                p_electron = random.uniform(2,8)  # electron momentum in GeV
+                theta_electron = random.uniform(15, 35)  # electron theta angle in degrees
+                phi_electron = random.uniform(-180, 180)
+                px_electron = p_electron * ROOT.TMath.Sin(theta_electron * ROOT.TMath.DegToRad()) * ROOT.TMath.Cos(phi_electron * ROOT.TMath.DegToRad())
+                py_electron = p_electron * ROOT.TMath.Sin(theta_electron * ROOT.TMath.DegToRad()) * ROOT.TMath.Sin(phi_electron * ROOT.TMath.DegToRad())
+                pz_electron = p_electron * ROOT.TMath.Cos(theta_electron * ROOT.TMath.DegToRad())                                                         
+                m_electron = 0.000511
+                e_electron = (px_electron**2 + py_electron**2 + pz_electron**2 + m_electron**2)**0.5
+                vx_electron = 0  # Vertex x-coordinate
+                vy_electron = 0  # Vertex y-coordinate
+                vz_electron = random.gauss(-3, 2.5)  # Random vertex z-coordinate with mean -3 cm and sigma 2.5 cm  
 
-                f.write(f"{index_el} {lifetime_el} {type_el} {particle_id_el} {index_parent_el} {index_daughter_el} {electron_px:.6f} {electron_py:.6f} {electron_pz:.6f} {electron_energy:.6f} {electron_mass} {electron_vx} {electron_vy} {electron_vz:.6f}\n")
-
+                f.write(f"{index_electron} {lifetime_electron} {type_electron} {particle_id_electron} {index_parent_electron} {index_daughter_elctron} {px_electron:.6f} {py_electron:.6f} {pz_electron:.6f} {e_electron:.6f} {m_electron} {vx_electron} {vy_electron} {vz_electron:.6f}\n")
                 # === Generate random parameters for the proton ===
-                index_prot = 2
-                lifetime_prot = 1 
-                type_prot = 1 # propagated in geant4
-                particle_id_prot = 2212
-                index_parent_prot = 0
-                index_daughter_prot = 0
-                proton_px = random.uniform(-0.5, 0.5)
-                proton_py = random.uniform(-0.5, 0.5)
-                proton_pz = random.uniform(0, 1.5) #??? not sure about what proton pz should be
-                proton_mass = 0.938
-                proton_energy = (proton_px**2 + proton_py**2 + proton_pz**2 + proton_mass**2)**0.5
-                proton_vx = electron_vx  # Both originate from the same vertex
-                proton_vy = electron_vy  # Both originate from the same vertex
-                proton_vz = electron_vz  # Both originate from the same vertex
+                index_proton = 2
+                lifetime_proton = 1 
+                type_proton = 1 # propagated in geant4
+                particle_id_proton = 2212
+                index_parent_proton = 0
+                index_daughter_proton = 0
+                p_proton = random.uniform(0.3,5)  # Proton momentum in GeV
+                theta_proton = random.uniform(3, 80)  # Proton theta angle
+                phi_proton = random.uniform(-180, 180)
+                px_proton = p_proton * ROOT.TMath.Sin(theta_proton * ROOT.TMath.DegToRad()) * ROOT.TMath.Cos(phi_proton * ROOT.TMath.DegToRad())
+                py_proton = p_proton * ROOT.TMath.Sin(theta_proton * ROOT.TMath.DegToRad()) * ROOT.TMath.Sin(phi_proton * ROOT.TMath.DegToRad())
+                pz_proton = p_proton * ROOT.TMath.Cos(theta_proton * ROOT.TMath.DegToRad())                                                            
+                m_protons = 0.938
+                e_proton = (px_proton**2 + py_proton**2 + pz_proton**2 + m_protons**2)**0.5
+                vx_proton = vx_electron  # Both originate from the same vertex
+                vy_proton = vy_electron  # Both originate from the same vertex
+                vz_proton = vz_electron  # Both originate from the same vertex
                 
-                f.write(f"{index_prot} {lifetime_prot} {type_prot} {particle_id_prot} {index_parent_prot} {index_daughter_prot} {proton_px:.6f} {proton_py:.6f} {proton_pz:.6f} {proton_energy:.6f} {proton_mass} {proton_vx} {proton_vy} {proton_vz:.6f}\n")
-
+                f.write(f"{index_proton} {lifetime_proton} {type_proton} {particle_id_proton} {index_parent_proton} {index_daughter_proton} {px_proton:.6f} {py_proton:.6f} {pz_proton:.6f} {e_proton:.6f} {m_protons} {vx_proton} {vy_proton} {vz_proton:.6f}\n")
         print(f"LUND file saved: {file_path}")
 
 # === Example usage ===
